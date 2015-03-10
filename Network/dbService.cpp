85a4f0d5d6a55fa17f7ebb2873b9b6c6e0aeb270
tony.geng <tony.geng@dianping.com>
2015-03-10 11:04:22 +0800

    remove the Network

--- Network/dbService.cpp
+++
@@ -1,9 +0,0 @@
-#include "dbService.h"
-#include "iostream"
-
-void dbService::run() {
-	std::cout << "run the dbService\n";
-}
-
-// Register the class, this is very important
-REGISTER_CLASS(dbService);
--- Network/service.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SERVICE_H
-#define __SERVICE_H
-
-class Service {
-public:
-	virtual void run() = 0;
-};
-
-#endif	// __SERVICE_H
